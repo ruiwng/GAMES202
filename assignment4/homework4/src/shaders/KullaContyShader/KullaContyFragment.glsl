d698877bde3fd5b4d27047102259d9c91c9203bf
rain.wr <rain.wr@alibaba-inc.com>
2022-11-29 19:05:38 +0800

    feat: Real-time Kulla-Conty BRDF

--- assignment4/homework4/src/shaders/KullaContyShader/KullaContyFragment.glsl
+++ assignment4/homework4/src/shaders/KullaContyShader/KullaContyFragment.glsl
@@ -23,27 +23,43 @@ const float PI = 3.14159265359;
 float DistributionGGX(vec3 N, vec3 H, float roughness)
 {
    // TODO: To calculate GGX NDF here
-    
+    float a = roughness * roughness;
+    float a2 = a * a;
+    float NdotH = max(dot(N, H), 0.0);
+    float NdotH2 = NdotH * NdotH;
+
+    float nom = a2;
+    float denom = (NdotH2 * (a2 - 1.0) +1.0);
+    denom = PI * denom * denom;
+
+    return nom / max(denom, 0.0001);
 }
 
 float GeometrySchlickGGX(float NdotV, float roughness)
 {
     // TODO: To calculate Schlick G1 here
-    
-    return 1.0;
+    float a = roughness;
+    float k = (a * a) / 2.0;
+
+    float nom = NdotV;
+    float denom = NdotV * (1.0 - k) + k;
+
+    return nom / denom;
 }
 
 float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
 {
     // TODO: To calculate Smith G here
+    float ggx2 = GeometrySchlickGGX(max(dot(N, V), 0.0), roughness);
+    float ggx1 = GeometrySchlickGGX(max(dot(N, L), 0.0), roughness);
 
-    return 1.0;
+    return ggx1 * ggx2;
 }
 
 vec3 fresnelSchlick(vec3 F0, vec3 V, vec3 H)
 {
     // TODO: To calculate Schlick F here
-    return vec3(1.0);
+    return F0 + (vec3(1.0) - F0) * pow(1.0 - max(dot(V, H), 0.0), 5.0);
 }
 
 
@@ -69,9 +85,9 @@ vec3 MultiScatterBRDF(float NdotL, float NdotV)
   
   // TODO: To calculate fms and missing energy here
 
-
-  return vec3(1.0);
-  
+  vec3 F_ms = (vec3(1.0) - E_o) * (vec3(1.0) - E_i) / (PI * (vec3(1.0) - E_avg));
+  vec3 F_add = F_avg * E_avg / (vec3(1.0) - F_avg * (vec3(1.0) - E_avg));
+  return F_ms * F_add;
 }
 
 void main(void) {
